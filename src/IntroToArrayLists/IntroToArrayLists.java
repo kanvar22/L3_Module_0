8da05cefa2609a8d261b7ff2b7f767a77076c469
100297 <league@WTS7.attlocal.net>
2017-09-07 19:59:52 -0700

    I finished Stack, HashMap, and ArrayList

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,31 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
 		
-		//2. Add five Strings to your list
+		ArrayList<String> names = new ArrayList<String>();
 		
-		//3. Print all the Strings using a standard for-loop
+		names.add("Bob");
+		names.add("Mark");
+		names.add("Jimmy");
+		names.add("Kanvar");
+		names.add("Brandon");
 		
-		//4. Print all the Strings using a for-each loop
 		
-		//5. Print only the even numbered elements in the list.
 		
-		//6. Print all the Strings in reverse order.
 		
-		//7. Print only the Strings that have the letter 'e' in them.
+		for(int i = 0; i < names.size(); i++){
+			String s = names.get(i);
+			System.out.println("String at element " + i + " " + s);
+		}
+		
+		
+
+		
+
+		
+
 	}
 }
--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,17 +1,35 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.print.attribute.standard.JobKOctetsProcessed;
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
+		Stack<Double> doubbble = new Stack<Double>();
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Random r = new Random();
+		for (int i = 0; i < 100; i++) {
+			doubbble.push(r.nextDouble()* 100);
+		}
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		String return1 = JOptionPane.showInputDialog(null, "Choose a number between 1 and 100.");
+		String return2 = JOptionPane.showInputDialog(null, "Choose a number between 1 and 100.");
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		double cost1 = Double.parseDouble(return1);
+		double cost2 = Double.parseDouble(return2);
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		for (int i = 0; i < 100; i++) {
+			double double1 = doubbble.pop();
+			if (double1 >= cost1 && double1 <= cost2) {
+			System.out.println("The number that you printed was " + double1);
+			}
+		}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
